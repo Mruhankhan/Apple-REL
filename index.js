42c4a9d9ee52d623c0614912773e03fb5f8b8194
Muhammad Khan <s1942405@online.houstonisd.org>
2023-05-11 23:45:58 -0500

    Work

--- index.js
+++ index.js
@@ -1,40 +1,50 @@
-let introducingNewPhonesText = document.getElementById('introducingNewPhonesText')
+let introducingNewPhonesText = document.getElementById(
+  "introducingNewPhonesText"
+);
 
 timeoutIntroducingNewPhonesText(1200)
   .then(() => {
-    let introducingNewPhonesText = document.getElementById("introducingNewPhonesText")
-    introducingNewPhonesText.innerText = "Introducing Our Newest Phones"
-    return timeoutIntroducingNewPhonesText(1400)
+    let introducingNewPhonesText = document.getElementById(
+      "introducingNewPhonesText"
+    );
+    introducingNewPhonesText.innerText = "Introducing Our Newest Phones";
+    return timeoutIntroducingNewPhonesText(1400);
   })
   .then(() => {
-    let introducingNewPhonesText = document.getElementById("introducingNewPhonesText")
-    introducingNewPhonesText.innerText = "Iphone 14 Pro For $1200"
-    return timeoutIntroducingNewPhonesText(1600)
+    let introducingNewPhonesText = document.getElementById(
+      "introducingNewPhonesText"
+    );
+    introducingNewPhonesText.innerText = "Iphone 14 Pro For $1200";
+    return timeoutIntroducingNewPhonesText(1600);
   })
   .then(() => {
-    let introducingNewPhonesText = document.getElementById("introducingNewPhonesText")
-    introducingNewPhonesText.innerText = "Samsung s23 Ultra For $1500"
-    return timeoutIntroducingNewPhonesText(1800)
+    let introducingNewPhonesText = document.getElementById(
+      "introducingNewPhonesText"
+    );
+    introducingNewPhonesText.innerText = "Samsung s23 Ultra For $1500";
+    return timeoutIntroducingNewPhonesText(1800);
   })
 
   .then(() => {
-    let introducingNewPhonesText = document.getElementById("introducingNewPhonesText")
-    introducingNewPhonesText.innerText = "Buy Fast Now On Sale!!!"
-    return timeoutIntroducingNewPhonesText(2000)
+    let introducingNewPhonesText = document.getElementById(
+      "introducingNewPhonesText"
+    );
+    introducingNewPhonesText.innerText = "Buy Fast Now On Sale!!!";
+    return timeoutIntroducingNewPhonesText(2000);
   })
-  .catch(e => {
-    console.error(e)
-    Promise.reject(e)
+  .catch((e) => {
+    console.error(e);
+    Promise.reject(e);
   })
   .then(() => {
     try {
-      Promise.resolve(timeoutIntroducingNewPhonesText())
+      Promise.resolve(timeoutIntroducingNewPhonesText());
     } catch {
-      Promise.reject(timeoutIntroducingNewPhonesText())
+      Promise.reject(timeoutIntroducingNewPhonesText());
     }
-  })
+  });
 function timeoutIntroducingNewPhonesText(time) {
-  return new Promise(resolve => {
-    setTimeout(resolve, time)
-  })
+  return new Promise((resolve) => {
+    setTimeout(resolve, time);
+  });
 }
